04c74af55e899c41c2c1840840c854a8a8632713
Brian Wilkinson <brian.wilkinson@fasthosts.com>
2018-02-05 16:53:48 +0000

    Use the new testpack helper library to provide common unittest functions

--- testpack/scripts/debian-9-mongo-3.py
+++ testpack/scripts/debian-9-mongo-3.py
@@ -9,13 +9,9 @@ from testpack_helper_library.unittests.dockertests import Test1and1Common
 
 
 class Test1and1MongoImage(Test1and1Common):
-    container_ip = None
-
     @classmethod
     def setUpClass(cls):
         Test1and1Common.setUpClass()
-        details = docker.APIClient().inspect_container(container=Test1and1Common.container.id)
-        Test1and1MongoImage.container_ip = details['NetworkSettings']['IPAddress']
         time.sleep(5) # Container needs time to start, stop, then restart mongodb before we test.
 
     # <tests to run>
